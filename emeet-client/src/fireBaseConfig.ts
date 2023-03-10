e56ee2b8df0b1a1685bdb530aedbd42aebf9a3f2
Phatcharaphon Keawprasit <6510110307@email.psu.ac.th>
2023-03-10 08:38:47 +0700

    Change the new SDK setup and configuration to deploy project

--- emeet-client/src/fireBaseConfig.ts
+++ emeet-client/src/fireBaseConfig.ts
@@ -8,14 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAPPurnapXdSbllfARcIds9JlgC4Uayl7M",
-  authDomain: "emeetfes.firebaseapp.com",
-  databaseURL: "https://emeetfes-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "emeetfes",
-  storageBucket: "emeetfes.appspot.com",
-  messagingSenderId: "632905686306",
-  appId: "1:632905686306:web:79482c59e442e6f3d0597b",
-  measurementId: "G-L0160MBSJP"
+  apiKey: "AIzaSyC6NNeluZetN9RZaULxdwRLGh-KOJrUVA0",
+  authDomain: "emeetfes-553a8.firebaseapp.com",
+  databaseURL: "https://emeetfes-553a8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "emeetfes-553a8",
+  storageBucket: "emeetfes-553a8.appspot.com",
+  messagingSenderId: "580497298910",
+  appId: "1:580497298910:web:d974ba7d678d659afcb94f",
+  measurementId: "G-QD8DS3CYBF"
 };
 
 // Initialize Firebase
